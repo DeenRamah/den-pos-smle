be9b35f4c533e70c773ffaecc53a00ab4091e65c
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-09-05 22:59:09 +0300

    Create multireplace.js

---
+++ multireplace.js
@@ -0,0 +1,6 @@
+//multiple replace
+var data = 'javascript is javascript'
+//single
+console.log(data.replace(/javascript/, 'typescript'))
+//multiple
+console.log(data.replace(/javascript/g, 'Ttypescript'))
