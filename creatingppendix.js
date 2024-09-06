b327550b60787cea7e378c7b9b94c8056ea7b70d
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-09-06 23:41:09 +0300

    Create creatingppendix.js

---
+++ creatingppendix.js
@@ -0,0 +1,5 @@
+document.createElement("tagName");
+document.createTextNode("text content")
+parentElement.appendChild(childElement);
+parentElement.insertBefore(newElement, referenceElement);
+parentElement.removeChild(childElement);
