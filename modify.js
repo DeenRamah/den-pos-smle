bdabfd9ff9c3f06c76cf913393c43a9d4d612a2c
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-09-06 23:32:37 +0300

    Create modify.js

---
+++ modify.js
@@ -0,0 +1,8 @@
+element.innerHTML;
+element.textContent;
+element.setAttribute("");
+element.getAttribute("");
+element.classList.add("class");
+element.classList.remove("class");
+element.classList.toggle("class");
+element.style.property ="Value";
