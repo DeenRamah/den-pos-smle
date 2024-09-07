0896929b52da2415aa00fdf8f50d0acf6d114be5
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-09-07 21:51:03 +0300

    Create queryelement.js

---
+++ queryelement.js
@@ -0,0 +1,9 @@
+//query by id
+const element = document.querySelector('#elementid');
+
+const element = document.querySelecrtorAll('#elemnt id');
+//by class name
+const element = document.querySelector('.className');
+
+//group by class Name
+const elements = document.querySelectorAll('.className');
